cecd3ee056aebe12a7c6a952dfc45369f89ce3f9
aljazg <aljazgrubar@gmail.com>
2021-02-28 12:41:17 +0100

    first commmit

---
+++ generateAscii.py
@@ -0,0 +1,97 @@
+import PIL.Image
+import os
+
+
+ASCII11 = ["@", "#", "S", "%", "?", "*", ";", ":", ",", ".", " "]
+
+
+# gray scale level values from:
+# http://paulbourke.net/dataformats/asciiart/
+
+ASCII70 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+
+
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height / width
+    new_height = int(new_width * ratio)
+    resized_image = image.resize((new_width, new_height))
+    return resized_image
+
+
+def img_to_gray(image):
+    gray = image.convert("L")
+    return gray
+
+
+def pixels_to_ascii(image, selected_ascii):
+    pixels = image.getdata()
+    asc = ASCII11
+
+    if selected_ascii == 2:
+        asc = ASCII70
+
+    chars = []
+    d = int(255 / len(asc))
+
+    for pixel in pixels:
+        idx = pixel // d
+        if idx >= len(asc):
+            idx = len(asc) - 1
+        chars.append(asc[idx])
+
+    return "".join(chars)
+
+
+def load_images_from_folder(folder):
+    images = []
+    names = []
+    for filename in os.listdir(folder):
+        img = PIL.Image.open(folder + filename)
+        if img is not None:
+            images.append(img)
+            base = os.path.basename(folder + filename)
+            names.append(os.path.splitext(base)[0])
+
+    return images, names
+
+
+def generate_ascii(image, name, ascii_dir, new_width, ascii):
+    new_image_data = pixels_to_ascii(img_to_gray(resize_image(image, new_width)), ascii)
+    count = len(new_image_data)
+
+    ascii_image = "\n".join(new_image_data[i:(i + new_width)] for i in range(0, count, new_width))
+
+    with open(ascii_dir + name + ".txt", "w") as f:
+        f.write(ascii_image)
+
+
+def main():
+    try:
+
+        images, names = load_images_from_folder("./photos/")
+        ascii_dir = "./generated_ascii/"
+
+        print("Enter desired width in px:")
+        new_width = int(input())
+
+        print("Choose desired ascii scheme (1 or 2, leave empty for default):")
+        ascii = input()
+
+        if ascii == '':
+            ascii = 1
+        else:
+            ascii = int(ascii)
+
+        if not os.path.exists(ascii_dir):
+            os.mkdir(ascii_dir)
+
+        for i in range(0, len(images)):
+            generate_ascii(images[i], names[i], ascii_dir, new_width, ascii)
+
+    except:
+        print("Stonks, looks like you did some dumb shit D: Read the --> readme.md <-- retard")
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,21 @@
+## The Grubko ascii img generator
+
+A dank ass python script designed to convert your legal anime lolli photos into more legal and clean ascii grills
+
+### Requirements
+
+* Python 3.6 or higher
+* The pillow package https://pillow.readthedocs.io/en/stable/
+* Hacking gloves
+
+### Usage
+
+1. make sure python and pip are installed
+2. run `pip install pillow`
+3. put desired photos in ./photos/
+4. run script: `python generateAscii.py`
+5. input the desired width of your ascii output (height will be scaled appropriately)
+6. input a number corresponding to desired ascii scheme (default is 1): 
+* 1 -> 11 ascii characters
+* 2 -> 70 ascii characters
+7. celebrate as your transformed ascii anime girls are waiting for you in "./generated_ascii/"
